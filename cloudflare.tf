938f142f72949dfb772cc44ce36a130d34cee5f8
Nanda Kishore <battaru.nandakishore@bigbasket.com>
2020-05-09 15:26:09 +0530

    Move to none-da.dev

--- cloudflare.tf
+++ cloudflare.tf
@@ -1,50 +1,47 @@
 provider "cloudflare" {
-  version           = "~> 1.18"
-  use_org_from_zone = "none-da.com"
+  version = "~> 1.18"
+  # email pulled from $CLOUDFLARE_EMAIL
+  # token pulled from $CLOUDFLARE_TOKEN
 }
 
-variable "domain" {
-  default = "none-da.com"
-}
-
-resource "cloudflare_record" "none-da-dot-com-A-1" {
-  domain  = "${var.domain}"
-  name    = "none-da.com"
+resource "cloudflare_record" "none-da-dot-dev-A-1" {
+  domain  = var.domain
+  name    = "none-da.dev"
   value   = "185.199.108.153"
   type    = "A"
   proxied = true
 }
-resource "cloudflare_record" "none-da-dot-com-A-2" {
-  domain  = "${var.domain}"
-  name    = "none-da.com"
+resource "cloudflare_record" "none-da-dot-dev-A-2" {
+  domain  = var.domain
+  name    = "none-da.dev"
   value   = "185.199.109.153"
   type    = "A"
   proxied = true
 }
-resource "cloudflare_record" "none-da-dot-com-A-3" {
-  domain  = "${var.domain}"
-  name    = "none-da.com"
+resource "cloudflare_record" "none-da-dot-dev-A-3" {
+  domain  = var.domain
+  name    = "none-da.dev"
   value   = "185.199.110.153"
   type    = "A"
   proxied = true
 }
-resource "cloudflare_record" "none-da-dot-com-A-4" {
-  domain  = "${var.domain}"
-  name    = "none-da.com"
+resource "cloudflare_record" "none-da-dot-dev-A-4" {
+  domain  = var.domain
+  name    = "none-da.dev"
   value   = "185.199.111.153"
   type    = "A"
   proxied = true
 }
 resource "cloudflare_record" "www" {
-  domain  = "${var.domain}"
+  domain  = var.domain
   name    = "www"
   value   = "none-da.github.io"
   type    = "CNAME"
   proxied = true
 }
 
-resource "cloudflare_zone_settings_override" "none-da-dot-com-settings" {
-  name = "${var.domain}"
+resource "cloudflare_zone_settings_override" "none-da-dot-dev-settings" {
+  name = var.domain
 
   settings {
     tls_1_3                  = "on"
@@ -58,8 +55,8 @@ resource "cloudflare_zone_settings_override" "none-da-dot-com-settings" {
 }
 
 resource "cloudflare_page_rule" "page_rule" {
-  zone   = "${var.domain}"
-  target = "http://${var.domain}/*"
+  zone_id = var.zone_id
+  target  = format("http://%s/*", var.domain)
   actions {
     always_use_https         = true
     email_obfuscation        = "on"
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable domain {
+  default = "none-da.dev"
+}
+
+variable zone_id {
+  default = "cc1635f8d56bf5d4ded853b5b1648a05"
+}
